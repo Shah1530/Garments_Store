396da8f35cd65d9c1223a07b7b54b18a5fd8dd75
azurecoders <azfarchannel54321@gmail.com>
2025-07-13 21:23:34 +0500

    Create Admin Page Added

--- modules/admin/user/user.route.js
+++ modules/admin/user/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
+  CreateNewAdminController,
   FetchAllUsersController,
   UpdateUserAccountStatusController,
 } from "./user.controller.js";
 
 const router = express.Router();
 
+router.post("/admin-create", CreateNewAdminController);
 router.get("/", FetchAllUsersController);
 router.put("/:id", UpdateUserAccountStatusController);
 
